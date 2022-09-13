9bdd60cfd70fb563b8ea655882008322374e8a85
AnimBadger <anim.ansah.stephen@gmail.com>
2022-09-13 23:51:53 +0000

    test C

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * print_last_number - driver function
+ * print_last_digit - driver function
  * @c: parameter for function
  *
  * Return: Last digit in number
  */
 
-int print_last_number(int c)
+int print_last_digit(int c)
 {
 	return (c % 10);
 }
