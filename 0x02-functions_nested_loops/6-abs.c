b8e452538660aa104deb9c801ceaf2d83f37eb25
AnimBadger <anim.ansah.stephen@gmail.com>
2022-09-13 23:24:29 +0000

    functions with c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.c"
+#include <stdlib.h>
+
+/**
+ * _abs - function driver
+ * @c: parameter passed
+ *
+ * Return: 0 means success
+ */
+
+int _abs(int c)
+{
+	final = abs(n);
+
+	return (final);
+}
