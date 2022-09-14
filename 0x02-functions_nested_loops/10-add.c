24ec99a3a9b3a841520139403afda0401396eff4
AnimBadger <anim.ansah.stephen@gmail.com>
2022-09-14 02:32:32 +0000

    functions with C

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - driver function
+ * @a: first parameter
+ * @b: second parameter
+ *
+ * Return: 0 means success
+ */
+
+int add(int a, int b)
+{
+	int total = a + b;
+
+	return (total);
+}
