e4da0bf5f4c3a0a6189861638a97264a494d55e8
AnimBadger <anim.ansah.stephen@gmail.com>
2022-09-14 21:49:55 +0000

    functions with C

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - driver function
+ *
+ * Return: 0 means success
+ */
+
+int main(void)
+{
+	int i;
+	long int = feb[50];
+
+	feb[0] = 1;
+	feb[1] = 2;
+
+	printf("%ld, %ld, ", feb[0], feb[1]);
+
+	for (i = 2; i < 50; i++)
+	{
+		feb[i] = feb[i - 1] + feb[n - 2];
+
+		if (i == 49)
+		{
+			printf("%ld\n", feb[i]);
+		}
+		else
+		{
+			printf("%ld, ", feb[i]);
+		}
+	}
+	return (0);
+}
