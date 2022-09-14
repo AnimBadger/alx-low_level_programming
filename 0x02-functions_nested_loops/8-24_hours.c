655949e12ce55e572fe1bfabbc0882db89ad2389
AnimBadger <anim.ansah.stephen@gmail.com>
2022-09-14 01:03:53 +0000

    fixed

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -14,12 +14,15 @@ void jack_bauer(void)
 	{
 		for (M = 0; M <= 60; M++)
 		{
-			_putchar(H / 10 + '0');
-			_putchar(H % 10 + '0');
-			_putchar(':');
-			_putchar(M / 10 + '0');
-			_putchar(M % 10 + '0');
-			_putchar('\n');
+			if (H != 24 && M != 60)
+			{
+				_putchar(H / 10 + '0');
+				_putchar(H % 10 + '0');
+				_putchar(':');
+				_putchar(M / 10 + '0');
+				_putchar(M % 10 + '0');
+				_putchar('\n');
+			}
 		}
 	}
 }
