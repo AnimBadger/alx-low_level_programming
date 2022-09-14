ef5deef3ee52b422d6895c81d4fbb9d489ed5014
AnimBadger <anim.ansah.stephen@gmail.com>
2022-09-14 00:50:33 +0000

    functions with C

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function driver;
+ *
+ * Return: 0 means succes;
+ */
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H <= 24; H++)
+	{
+		for (M = 0; M <= 60; M++)
+		{
+			_putchar(M / 10 + '0');
+			_putchar(M % 10 + '0');
+			_putchar(':');
+			_putchar(H / 10 + '0');
+			_putchar(H % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
