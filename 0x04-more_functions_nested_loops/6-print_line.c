6e5762aad845e5bdfe10152f6d31c94af112f937
AnimBadger <anim.ansah.stephen@gmail.com>
2022-09-16 00:39:13 +0000

    bug fix

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -11,16 +11,14 @@ void print_line(int n)
 {
 	int times;
 
-	for (times = 0; times <= n; times++)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
-		{
-			_putchar('_');
-		}
+		_putchar('\n');
+	}
+
+	for (times = 1; times <= n; times++)
+	{
+		_putchar('_');
 	}
 	_putchar('\n');
 }
