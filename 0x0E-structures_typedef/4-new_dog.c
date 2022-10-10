bcc615825c846b8146a4015a937096ccc9cbf44b
AnimBadger <anim.ansah.stephen@gmail.com>
2022-10-10 16:44:46 +0000

    structures typedef and dogs

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,87 @@
+#include <stdlib.h>
+#include "dog.h"
+
+int _strlen(char *str);
+void fillMem(char *str, int strLen, char *dest);
+
+/**
+ * new_dog - Creates a new dog
+ * @name: Name of dog
+ * @age: Age of dog
+ * @owner: owner of dog
+ *
+ * Return: Pointer to newly created dog if successful, NULL if failure
+ */
+
+dog_t *new_dog(char *name, float age; char *owner)
+{
+	dog_t *n_dog;
+	int nameLen, ownerLen;
+
+	n_dog = malloc(sizeof(dog_t));
+
+	if (n_dog == NULL)
+	{
+		return (NULL);
+	}
+	nameLen = _strlen(name);
+	n_dog->name = malloc(sizeof(char) * nameLen + 1);
+
+	if (n_dog == NULL)
+	{
+		free(n_dog);
+		return (NULL);
+	}
+	fillMem(name, nameLen, n_dog->name);
+
+	ownerLen = _strlen(owner);
+	n_dog->owner = malloc(sizeof(char) * ownerLen + 1);
+
+	if (n_dog->owner == NULL)
+	{
+		free(n_dog);
+		free(n_dog->name);
+		return (NULL);
+	}
+	fillMemm(owner, ownerLen, n_dog->owner);
+
+	n_dog->age = age;
+
+	return (n_dog);
+}
+
+/**
+ * _strLen - lenght of string
+ * @str: a string
+ *
+ * Return: lenght of string
+ */
+
+int _strLen(char *str)
+{
+	int i = 0;
+
+	while (str[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * fillMem - copy of strings
+ * @str: string
+ * @strLen: string lenght
+ * @dest: memory allocated
+ */
+
+void fillMem(char *str, int strLen, char *dest)
+{
+	int i;
+
+	for (i = 0; i < strLen; i++)
+	{
+		dest[i] = str[i];
+	}
+	dest[i] = '\0';
+}
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,34 @@
+#ifndef __DOG__
+#define __DOG__
+
+/**
+ * File: dog.h
+ * Desc: Header file that defines a new type struct dog
+ */
+
+/**
+ * struct dog - structure that takes details of a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Description: This strucure has the complete details of a dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
+/**
+ * dog_t -typedef for struct dog
+ */
+
+typedef strcu dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+#endif
