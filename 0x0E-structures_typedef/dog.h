31dbb4e0867921ecaa7aa160c3c3cb96021987e4
AnimBadger <anim.ansah.stephen@gmail.com>
2022-10-10 20:35:17 +0000

    fix?

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,20 +15,10 @@
  * Description: This strucure has the complete details of a dog
  */
 
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
-/**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 #endif
