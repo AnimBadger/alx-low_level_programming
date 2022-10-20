45ffaa0c7509ec10eb67e43a0fcedcb9042d5462
AnimBadger <anim.ansah.stephen@gmail.com>
2022-10-20 15:12:50 +0000

    length of string fix

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -17,8 +17,26 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	temp->str = strdup(str);
+	temp->len = string_length(str);
 	temp->next = *head;
 	*head = temp;
 
 	return (*head);
+}
+
+/**
+ * string_length - length of the string
+ * @pointer: pointer of string
+ * Return: length
+ */
+
+int string_length(const char *pointer)
+{
+	int counter = 0;
+
+	while (*(pointer + counter) != '\0')
+	{
+		counter++;
+	}
+	return (counter);
 }
