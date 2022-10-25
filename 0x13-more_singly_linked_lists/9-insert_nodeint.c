8ac16267bc492af605a1328a9a32455f604337da
AnimBadger <anim.ansah.stephen@gmail.com>
2022-10-25 12:08:29 +0000

    insertion fix

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,9 +22,19 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	new_node->n = n;
 	new_node->next = NULL;
+	if (idx == 0)
+	{
+		new_node->next = temp;
+		*head = new_node;
 
+		return (new_node);
+	}
 	for (count = 0; count != (idx - 1); count++)
 	{
+		if (temp == NULL || temp->next == NULL)
+		{
+			return (NULL);
+		}
 		temp = temp->next;
 	}
 	new_node->next = temp->next;
