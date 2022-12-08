41d686e746e01bc1407fa85a2a93ab4213b0273a
AnimBadger <Anim.ansah.stephen@gmail.com>
2022-12-08 18:27:47 +0000

    find node

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function to get node
+ * @head: head of list
+ * @index: index to fetch from list 
+*/
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int track;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (head->prev != NULL)
+	{
+		head = head->prev;
+	}
+	track = 0;
+
+	while (head != NULL)
+	{
+		if (track == index)
+		{
+			break;
+		}
+		head = head->next;
+		track++;
+	}
+	return (head);
+}
